fdc834fea6d022fee9d2b1d63614d3c5828b19b9
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-03 22:58:08 +0300

    added task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
     }
 
     public  static void task1(){
@@ -8,6 +9,21 @@ public class Main {
         if (clientOS == 1){
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+    public static  void task2(){
+        int clientOS = 0;
+        int clientDeviceYear = 2004;
+        boolean deviceIsOld = clientDeviceYear < 2015;
+        if (clientOS == 1 && deviceIsOld){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && !deviceIsOld) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && deviceIsOld) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && !deviceIsOld) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
     }
