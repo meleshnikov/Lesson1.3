c07e0fdd75baab8f20348c5d58fe943df93dc5fe
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-03 22:23:01 +0300

    added task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public  static void task1(){
+        int clientOS = 0;
+        if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
     }
 }
